be7517fa951ff53efcac5934eedc8ea5b8866f03
GonzaPiccinini <93500621+GonzaPiccinini@users.noreply.github.com>
2022-01-27 20:53:41 -0300

    feat(Root): Agregando firebase

---
+++ config/database.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: `${process.env.API_KEY}`,
+  authDomain: "eccomerce-191fc.firebaseapp.com",
+  projectId: "eccomerce-191fc",
+  storageBucket: "eccomerce-191fc.appspot.com",
+  messagingSenderId: "405544233558",
+  appId: `${process.env.APP_ID}`,
+  measurementId: `${process.env.MEASUREMENT_ID}`
+}
+
+const app = initializeApp(firebaseConfig);
