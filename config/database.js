bcadf614b473e796d9e2e26b830da2a50a0af80c
GonzaPiccinini <93500621+GonzaPiccinini@users.noreply.github.com>
2022-01-27 21:58:39 -0300

    feat(Root): Mostramos coleccion de productos de base de datos

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"
+import { getFirestore } from '@firebase/firestore'
 
 const firebaseConfig = {
   apiKey: `${process.env.API_KEY}`,
@@ -11,5 +12,4 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
-
-export default app
+export const db = getFirestore(app)
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react"
+import { db } from "../config/database"
+import { collection, getDocs } from 'firebase/firestore'
+
 export default function Home() {
+  const [products, setProducts] = useState([])
+  const productsCollection = collection(db, 'productos')
+  
+  useEffect(() => {
+
+    const getProducts = async () => {
+      const data = await getDocs(productsCollection)
+      setProducts(data.docs.map(doc => ({...doc.data(), id: doc.id})))
+    }
+
+    getProducts()
+  }, [])
+
   return (
-    <div className="text-blue-500 text-5xl text-center">Hola</div>
+    <div>
+      {products.map(product => <h1 key={product.id}>{product.name}</h1>)}
+    </div>
   )
 }
